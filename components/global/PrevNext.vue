6e43e39f7aac5ed4fd188602a9f69c2fdd8f6a91
campbell000 <ac.simpledex@gmail.com>
2020-11-14 19:48:57 -0500

    Corrected paginatino

--- components/global/PrevNext.vue
+++ components/global/PrevNext.vue
@@ -2,20 +2,20 @@
 <v-container class="mb-2">
   <v-row>
     <v-col :lg="6" class="nextPrevLink next">
-      <NuxtLink
+            <NuxtLink
+        v-if="prev"
+        :to="{ name: 'slug', params: { slug: prev.slug } }"
+        >
+        Earlier Post: {{ prev.title }}
+      </NuxtLink>
+    </v-col>
+    <v-col :lg="6" class="nextPrevLink previous">
+            <NuxtLink
         v-if="next"
         :to="{ name: 'slug', params: { slug: next.slug } }"
       >
                   
-        Previous: {{ next.title }}
-      </NuxtLink>
-    </v-col>
-    <v-col :lg="6" class="nextPrevLink previous">
-      <NuxtLink
-        v-if="prev"
-        :to="{ name: 'slug', params: { slug: prev.slug } }"
-        >
-        Next: {{ prev.title }}
+        Next Post: {{ next.title }}
       </NuxtLink>
     </v-col>
   </v-row>
